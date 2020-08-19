8f627dd669052288b72ce165b9c3d444cf7b7186
Mohit-Kulkarni <51435460+Mohit-Kulkarni@users.noreply.github.com>
2020-08-19 16:44:55 +0530

    initial upload

---
+++ new.js
@@ -0,0 +1,30 @@
+// jshint esversion: 6
+var noBunk = document.getElementById('ip1');
+
+noBunk.addEventListener("mouseover", function() {
+  var rdm = (Math.floor(Math.random() * 100)) + 1;
+  var minusPlus = (Math.floor(Math.random() * 2) + 1);
+  if (minusPlus == 1) {
+    this.style.marginLeft = rdm + "em";
+  } else {
+    this.style.marginLeft = -rdm + "em";
+  }
+
+});
+
+noBunk.addEventListener("click", function() {
+  if (typeof window.navigator.vibrate !== 'function') return
+  setInterval(() => {
+    const duration = Math.floor(Math.random() * 600)
+    window.navigator.vibrate(duration)
+    console.log("I got vibrated");
+  }, 1000);
+  alert("Ek baar bola Class nahi karneka phir bhi kaisa kya himmat hui re teri No Bunk pe click karne ki");
+});
+
+document.getElementById('ip2').addEventListener("click", function() {
+
+
+  alert("Tu Badlega Desh (Now I wonder what would have happened if I managed clicked the No bunk button)");
+
+});
